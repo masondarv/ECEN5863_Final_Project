7275a20018f6957d430d06bb61279b7519fb60f6
masondarv <robert.darveaux@colorado.edu>
2019-05-01 00:38:28 -0600

    collision implemented

--- collision.v
+++ collision.v
@@ -3,11 +3,37 @@
 // Pong Final Project
 
 module collision(
-	input clk, reset,
+	input clk, rst,
 	input paddle_1_sig,
 	input paddle_2_sig,
 	input ball_sig,
 	input frame_sig,
-	output collision);
+   input vsync,
+	output reg collision);
 
-endmodule // frame
+always @(posedge clk or posedge rst or negedge vsync)
+begin
+    if(rst) begin
+    collision <=0;
+
+    end
+	 
+	 //if vsync has gone low signalling end of frame, set collision equal to 0, the change will not be noticed until the 
+	 //next vsinc signal
+	 else if (!vsync) begin
+			collision <=0;
+	 
+	 end
+
+    else begin
+        if ((ball_sig & paddle_1_sig) || (ball_sig & paddle_2_sig) || ball_sig & frame_sig ) begin
+            collision <= 1;
+        end
+    end
+	 
+	 
+end
+
+endmodule
+
+
